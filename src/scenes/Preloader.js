c1c433b7edc90da0abaa72001b94f3fd47738b7f
Daniel Do <do2daniel@yahoo.com>
2023-06-10 00:05:12 -0700

    Implemented scenes and sprites

---
+++ src/scenes/Preloader.js
@@ -0,0 +1,80 @@
+class Preloader extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: 'preloader' });
+    }
+
+    preload ()
+    {
+        this.sys.canvas.style.display = 'block';
+
+        // just a preload bar in graphics
+        let progress = this.add.graphics();
+        this.load.on('progress', (value) => {
+            progress.clear();
+            progress.lineStyle(2, window.fgColor.color, 1);
+            progress.strokeRect((this.scale.width / 2) - 132, (this.scale.height / 2) - 20, 264, 40);
+            progress.fillStyle(window.fgColor.color, 1);
+            progress.fillRect((this.scale.width / 2) - 128, (this.scale.height / 2) - 16, 256 * value, 32);
+        });
+        this.load.on('complete', () => {
+            progress.destroy();
+        });
+
+        // Load assets here
+        // ...
+        this.load.image('napie-eight-font', 'assets/napie-eight-font.png');
+        this.load.tilemapTiledJSON('map', 'assets/map.json');
+        //this.load.image('tiles', 'assets/tiles.png');
+        this.load.image('tiles', 'assets/tiles-extruded.png');
+        this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.image('staminabar-outer', 'assets/staminabar-outer.png');
+        this.load.image('staminabar-center', 'assets/staminabar-center.png');
+        this.load.image('staminabar-inner', 'assets/staminabar-inner.png');
+        this.load.audio('buzz', 'assets/buzz.mp3');
+        this.load.audio('eerie', 'assets/eerie.mp3');
+        this.load.audioSprite('ping', 'assets/ping.json', 'assets/ping.mp3');
+        this.load.audioSprite('fail', 'assets/fail.json', 'assets/fail.mp3');
+        this.load.audioSprite('success', 'assets/success.json', 'assets/success.mp3');
+        this.load.audioSprite('hit', 'assets/hit.json', 'assets/hit.mp3');
+        this.load.spritesheet('particles', 'assets/particles.png', { frameWidth: 4, frameHeight: 4 });
+        /*
+        this.load.audio('music', 'assets/music.mp3');
+        this.load.audioSprite('jump', 'assets/jump.json', 'assets/jump.mp3');
+        */
+        /*
+        */
+    }
+
+    create ()
+    {
+        let fontConfig = {
+            image: 'napie-eight-font',
+            width: 8,
+            height: 8,
+            chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ .,!?#abcdefghijklmnopqrstuvwxyz@:;^%&1234567890*\'"`[]/\\~+-=<>(){}_|$',
+            charsPerRow: 16,
+            spacing: { x: 0, y: 0 }
+        };
+        this.cache.bitmapFont.add('napie-eight-font', Phaser.GameObjects.RetroFont.Parse(this, fontConfig));
+
+        /*
+        this.load.on('complete', () => {
+            this.load.off('complete');
+            this.ambient.allMusicLoaded = true;
+        }, this);
+        this.load.audio('music2', 'assets/music2.mp3');
+        this.load.audio('music3', 'assets/music3.mp3');
+        this.load.start();
+        */
+
+        this.scene.start('title');
+        //this.scene.start('level');
+        //this.scene.start('startscreen');
+        //this.scene.start('endscreen');
+    }
+
+}
+
+export default Preloader;
---
+++ src/spirtes/Player.js
@@ -0,0 +1,185 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y, key, frame, facing) {
+        super(scene, x, y, key, frame);
+        scene.physics.world.enable(this);
+        scene.add.existing(this);
+
+        this.setDepth(2);
+        this.setSize(16, 12, true);
+        this.setOffset(8, 18, true);
+        this.setCollideWorldBounds(true);
+        this.body.allowGravity = true;
+
+        // tweak stuff
+        this.speedMax = 96;
+        this.speedChange = 8;
+        this.maxFlyPower = 192;
+        this.flyChange = 8;
+        this.minStamina = 4000;
+        this.stamina = this.minStamina;
+        this.maxStamina = 24000;
+
+        // not tweakable
+        this.facing = facing || 'right';
+        this.idle = false;
+        this.moveSpeed = 0;
+        this.ani = 'idle-left';
+        this.alive = true;
+        this.flyPower = 0;
+        this.flyTimer = 0;
+        this.restTimer = this.stamina;
+        this.wasFlying = false;
+
+        var animations = [
+            { key: 'idle-left', start: 0, end: 0 },
+            { key: 'idle-right', start: 1, end: 1 },
+            { key: 'run-left', start: 2, end: 4 },
+            { key: 'run-right', start: 5, end: 7 },
+            { key: 'fly-left', start: 8, end: 10 },
+            { key: 'fly-right', start: 11, end: 13 },
+            { key: 'dead-left', start: 14, end: 16 },
+            { key: 'dead-right', start: 17, end: 19 }
+        ];
+        animations.forEach(this.addAnim.bind(this));
+    }
+
+    addAnim(anim)
+    {
+        let anims = this.anims.animationManager;
+        if (!anims.get(anim.key)) {
+            anims.create({
+                key: anim.key,
+                frames: anims.generateFrameNumbers('player', { start: anim.start, end: anim.end }),
+                frameRate: 15,
+                repeat: -1
+            });
+        }
+    }
+
+    update(controls, time, delta)
+    {
+        if (!this.alive) {
+            this.body.velocity.x = 0;
+            if (this.facing === 'left') {
+                this.ani = 'dead-left';
+            } else {
+                this.ani = 'dead-right';
+            }
+            this.anims.play(this.ani, true);
+            let onFloor = (this.body.onFloor() || this.body.touching.down);
+            if (onFloor) {
+                this.scene.gameOver();
+            }
+            return;
+        }
+
+        this.stamina = Math.max(this.minStamina, Math.min(this.maxStamina, Math.ceil(this.minStamina + ((this.maxStamina - this.minStamina) * (this.scene.points / this.scene.maxPoints)))));
+
+        this.runAndFly(controls, time, delta);
+
+        // don't forget to animate :)
+        this.anims.play(this.ani, true);
+    }
+
+    runAndFly(controls, time, delta)
+    {
+        if (controls.left) {
+
+            this.body.velocity.x -= this.speedChange;
+            this.body.velocity.x = Math.max(this.body.velocity.x, -this.speedMax);
+
+            this.facing = 'left';
+            this.idle = false;
+
+        } else if (controls.right) {
+
+            this.body.velocity.x += this.speedChange;
+            this.body.velocity.x = Math.min(this.body.velocity.x, this.speedMax);
+
+            this.facing = 'right';
+            this.idle = false;
+
+        } else {
+
+            this.body.velocity.x -= this.body.velocity.x / 2;
+
+            this.idle = true;
+
+        }
+
+        if (controls.aDown && this.flyTimer < this.stamina && this.restTimer > 0) {
+            this.flyPower += this.flyChange;
+            if (this.flyPower > this.maxFlyPower) {
+                this.flyPower = this.maxFlyPower;
+            }
+            this.body.setVelocityY(-this.flyPower);
+        } else {
+            this.flyPower = 0;
+        }
+
+        let onFloor = (this.body.onFloor() || this.body.touching.down);
+
+        if (onFloor) {
+
+            if (this.wasFlying) {
+                this.wasFlying = false;
+                this.scene.cameras.main.shake(250, 0.03);
+                this.scene.sfx.play('hit');
+                this.scene.groundParticlesEmitter.explode(20, this.body.x + 8, this.body.bottom);
+            }
+
+            if (this.restTimer >= this.stamina) {
+                this.restTimer = this.stamina;
+                this.flyTimer = 0;
+            } else {
+                this.restTimer += delta;
+                this.flyTimer -= delta;
+            }
+
+            if (this.idle) {
+
+                if (this.facing === 'left') {
+                    this.ani = 'idle-left';
+                } else {
+                    this.ani = 'idle-right';
+                }
+
+            } else {
+
+                if (this.facing === 'left') {
+                    this.ani = 'run-left';
+                } else {
+                    this.ani = 'run-right';
+                }
+
+            }
+
+        } else {
+
+            if (this.restTimer <= 0) {
+                this.restTimer = 0;
+                this.flyTimer = this.stamina;
+                if (!this.scene.isRestarting) {
+                    this.alive = false;
+                    this.scene.ambient.stop();
+                    this.scene.sfx.play('fail');
+                }
+            } else {
+                this.restTimer -= delta;
+                this.flyTimer += delta;
+            }
+
+            if (this.facing === 'left') {
+                this.ani = 'fly-left';
+            } else {
+                this.ani = 'fly-right';
+            }
+
+            this.wasFlying = true;
+
+        }
+    }
+}
+
+export default Player;
