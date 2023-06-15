ed9b5f90e9846641baf0ef1046c9c68cee08bd16
Daniel Do <do2daniel@yahoo.com>
2023-06-15 04:33:58 -0700

    Separated main config from GameScene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -26,14 +26,12 @@ class Credits extends Phaser.Scene {
         // show menu title text
         this.add.text(game.config.width/2, 40, 'CREDITS', menuKeyConfig).setOrigin(0.5);
 
-        // show instructions text
-        menuKeyConfig.align = 'left';
+        // show instructions text 
         menuKeyConfig.fontSize = '15px';
         menuKeyConfig.color = '#FFFFFF';
-        this.add.text(game.config.width/2, 100, 'Programming\n\nArt\n\nSound Effects\n\nMusic', menuKeyConfig).setOrigin(0.5);
-        menuKeyConfig.align = 'right';
+        this.add.text(game.config.width/2, 150, 'Programming\n\n\nArt & Design\n\n\nSound Effects\n\n\nMusic', menuKeyConfig).setOrigin(0.5);
         menuKeyConfig.color = '#A9A9A9';
-        this.add.text(game.config.width/2, 100, '\nDaniel Do\n\nDaniel Do\n\nPixabay.com\n\n"Ancient Dragon - Classic JRPG Music Pack" by Rest!', menuKeyConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 150, '\n\n\nDaniel Do\n\n\nDaniel Do\n\n\nPixabay.com\n\n\n"Ancient Dragon -\nClassic JRPG Music Pack" by Rest!', menuKeyConfig).setOrigin(0.5);
         menuKeyConfig.align = 'center';
         menuKeyConfig.color = '#FFD700';
         this.add.text(game.config.height/2, game.config.height/2 + 130, 'Press B to go back', menuKeyConfig).setOrigin(0.5);
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -124,24 +124,6 @@ class GameScene extends Phaser.Scene {
     draw() {
     }
 }
-var config = {
-    type: Phaser.AUTO,
-    width: 320,
-    height: 320,
-    scaleMode: 3,
-    pixelArt: true,
-    backgroundColor: '#000000',
-    parent: 'Fireflies',
-    title: "Fireflies",
-    version: "0.0.1",
-    disableContextMenu: true,
-    scene: [Menu, GameScene, Credits],
-};
-var game = new Phaser.Game(config);
-let keySPACE;
-let keyP;
-let keyC;
-let keyB;
 class TimeManager {
     constructor() { }
     static initialize() {
